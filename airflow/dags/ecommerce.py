7c45e1a3138cf8c65ec464ba1cb662d13bd3c7e2
Emmanuel Aminu <aminuemmazon@gmail.com>
2024-08-11 21:33:58 +0100

    Extract and Load

--- airflow/dags/ecommerce.py
+++ airflow/dags/ecommerce.py
@@ -1,44 +1,73 @@
-from airflow.decorators import DAG, tasks
-from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator 
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-from datetime import datetime 
+# from airflow.decorators import dag, task
+# from airflow.operators.postgres_operator import PostgresOperator
+# from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+# from datetime import datetime, timedelta
+# import pendulum
 
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.models import Variable
-from datetime import datetime, timedelta 
-import os
+# ## BigQuery config variables
+# BQ_CONN_ID = "google_cloud_default"  # Defined in airflow connection
+# BQ_BUCKET = 'order_ecomms'
 
+# ## Postgres config variables
+# PG_CONN_ID = "postgres_default"  # Defined in airflow connection
+# PG_SCHEMA = "IDX-Schema"
+# PG_TABLE1 = "olist.order_items"
+# PG_TABLE2 = "olist.order_payments"
+# PG_TABLE3 = "olist.orders"
+# PG_TABLE4 = "olist.order_reviews"
+# PG_TABLE5 = "olist.customers"
+# PG_TABLE6 = "olist.products"
+# PG_TABLE7 = "olist.sellers"
+# PG_TABLE8 = "olist.product_category"
+# PG_TABLE9 = "olist.product_category_name_translation"
 
+# tables = ["customers", "orders", "order_reviews", "sellers", "product_category"]
 
-SQL_SELECT = ""
-CONNECTION_ID = ""
-BUCKET_NAME= ""
-FILE_NAME =""
+# # (Other tables can be added as needed)
 
-default_args = {
-    "owner": "airflow",
-    "catchup": False,
-}
-
-@dag(
-    start_date=datetime(2023,1,2),
-     schedule=None,
-     catchup=False,
-     tags=['retail']
-     )
-
-@task 
-# get_data = PostgresToGCSOperator(
-#         task_id="upload_to_gcs",
-#         postgres_conn_id=CONNECTION_ID,
-#         sql=SQL_SELECT,
-#         bucket=BUCKET_NAME,
-#         filename=FILE_NAME,
-#         gzip=False,
-#     )
-
-
-# load_to_bq = GCSToBigQueryOperator(
-
+# @dag(
+#     schedule=None,
+#     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),  # Use pendulum for timezone-aware datetime
+#     catchup=False,
+#     tags=["example"],
 # )
+# def example_dag():
+
+#     @task
+#     def transfer_postgres_stock_to_gcs():
+#         # Task to transfer stock data from PostgreSQL to GCS
+#         return PostgresToGCSOperator(
+#             task_id='postgres_stock_to_gcs',
+#             sql=f'SELECT * FROM "{PG_SCHEMA}"."{PG_TABLE1}";',
+#             bucket=BQ_BUCKET,
+#             filename=f"{}"
+#             export_format='CSV',
+#             postgres_conn_id=PG_CONN_ID,
+#             field_delimiter=',',
+#             gzip=False,
+#             gcp_conn_id=BQ_CONN_ID,
+#         )
+
+#     @task
+#     def transfer_postgres_company_to_gcs():
+#         # Task to transfer company data from PostgreSQL to GCS
+#         return PostgresToGCSOperator(
+#             task_id='postgres_company_to_gcs',
+#             sql=f'SELECT * FROM "{PG_SCHEMA}"."{PG_TABLE2}";',
+#             bucket=BQ_BUCKET,
+#             filename=JSON_FILENAME2,
+#             export_format='CSV',
+#             postgres_conn_id=PG_CONN_ID,
+#             field_delimiter=',',
+#             gzip=False,
+#             gcp_conn_id=BQ_CONN_ID,
+#         )
+
+#     # Define the order of tasks
+#     stock_task = transfer_postgres_stock_to_gcs()
+#     company_task = transfer_postgres_company_to_gcs()
+
+#     stock_task >> company_task
+
+# dag = example_dag()
+
